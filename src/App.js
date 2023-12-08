f327f3add0075b96de006bb1b91580fd8b6926d3
Wilson Rodrigues <wilson.corleto@hotmail.com>
2023-12-08 18:10:05 -0300

    subindo formulário de cadastro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import VideoList from './ui/VideoList';
+import NewVideoForm from './ui/NewVideoForm';
+
 function App() {
   return (
     <div className="App">
+      <NewVideoForm />
       <VideoList />
     </div>
   );
--- src/ui/NewVideoForm.jsx
+++ src/ui/NewVideoForm.jsx
@@ -1,6 +1,27 @@
+import React, { useState } from "react"
 
 export default function NewVideoForm() {
+
+    const [title, setTitle] = useState('');
+    const [duration, setDuration] = useState('');
+    const [cover, setCover] = useState('');
+    const [url, setUrl] = useState('');
+
+    
+
     return(
-        "teste"
+        <div className="form">
+            <label htmlFor="title">Título:</label>
+            <input id="title" type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+
+            <label htmlFor="duration">duration:</label>
+            <input id="duration" type="text" value={duration} onChange={(event) => setDuration(event.target.value)} />
+
+            <label htmlFor="cover">cover:</label>
+            <input id="cover" type="text" value={cover} onChange={(event) => setCover(event.target.value)} />
+
+            <label htmlFor="url">url:</label>
+            <input id="url" type="text" value={url} onChange={(event) => setUrl(event.target.value)} />
+        </div>
     )
 }
